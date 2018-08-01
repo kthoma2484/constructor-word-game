24fab9e0a71fbdd11d9d93143f31da50aae8563d
KKT84 <35786891+kthoma2484@users.noreply.github.com>
2018-07-31 17:16:46 -0700

    edited letters js and started word js

--- letter.js
+++ letter.js
@@ -16,31 +16,32 @@ function Letter(name) {
 
     // A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
     this.letterGuess = function () {
-        //  console.log(word)
-        this.letterCheck();
-        //  console.log(guessed)
-        if (guessed === false) {
+        if (!userGuess) {
+            return null;
+        } else if (this.guessed === false) {
             console.log(hiddenLetter);
         } else {
             console.log(this.name);
-            // wordLetter = new Letter(userGuess);
-            // console.log(wordLetter.name);
         };
     };
 
     // A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
 
     this.letterCheck = function () {
-        if (word.indexOf(userGuess) == -1) {
-            guessed = false;
+        if (!userGuess) {
+            console.log('Please make a letter guess.');
+            return null;
+        } else if (word.indexOf(userGuess) == -1) {
+            this.guessed = false;
         } else {
-            guessed = true;
+            this.guessed = true;
         };
-        // console.log(guessed)
+       // console.log(this.guessed)
     };
 
 };
 
-let letterA = new Letter(userGuess)
+let letterA = new Letter(userGuess);
 
+letterA.letterCheck();
 letterA.letterGuess();
--- word.js
+++ word.js
@@ -1,11 +1,24 @@
-var inquirer = require("inquirer")
+var inquirer = require("inquirer");
 
+var Letter = require("./letter");
 
+let randomWord = "choosen";
 
-// An array of new Letter objects representing the letters of the underlying word
+//let wordArray = [];
 
+//console.log(wordArray);
+function Word() {
+    // An array of new Letter objects representing the letters of the underlying word
+    let hiddenLetters = [];
 
-// A function that returns a string representing the word. This should call the function on each letter object (the first function defined in Letter.js) that displays the character or an underscore and concatenate those together.
+    // A function that returns a string representing the word. This should call the function on each letter object (the first function defined in Letter.js) that displays the character or an underscore and concatenate those together.
+    this.wordLetters = function() {
+        wordArray = randomWord.split("");
 
+        Letter.letterGuess();
 
-// A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in Letter.js)
+        hiddenLetters.join(' ');
+    }
+    // A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in Letter.js)
+
+}
